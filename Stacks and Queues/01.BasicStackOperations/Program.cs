dd083917657495ae9454ad09686ab08edca71afc
Banovvv <ivan_banov@abv.bg>
2022-04-05 21:32:57 +0300

    Two solutions added

--- Stacks and Queues/01.BasicStackOperations/Program.cs
+++ Stacks and Queues/01.BasicStackOperations/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01.BasicStackOperations
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[] nsx = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int elementsToAdd = nsx[0];
+            int elementsToPop = nsx[1];
+            int elementToLookFor = nsx[2];
+
+            var elements = Console.ReadLine().Split(' ');
+
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < elementsToAdd; i++)
+            {
+                int element = int.Parse(elements[i]);
+
+                numbers.Push(element);
+            }
+
+            for (int i = 0; i < elementsToPop; i++)
+            {
+                numbers.Pop();
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (numbers.Contains(elementToLookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
         }
     }
 }
--- Stacks and Queues/02.BasicQueueOperations/Program.cs
+++ Stacks and Queues/02.BasicQueueOperations/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.BasicQueueOperations
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int[] nsx = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int elementsToQueue = nsx[0];
+            int elementsToDequeue = nsx[1];
+            int elementToLookFor = nsx[2];
+
+            int[] elements = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            for (int i = 0; i < elementsToQueue; i++)
+			{
+                numbers.Enqueue(elements[i]);
+			}
+
+            for (int i = 0; i < elementsToDequeue; i++)
+			{
+                numbers.Dequeue();
+			}
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (numbers.Contains(elementToLookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
         }
     }
 }
