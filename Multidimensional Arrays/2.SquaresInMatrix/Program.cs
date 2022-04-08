30ac8085b466c4f5b49b94135d4b6866945a498d
Banovvv <ivan_banov@abv.bg>
2022-04-08 13:31:33 +0300

    2. Squares In Matrix

--- Multidimensional Arrays/2.SquaresInMatrix/Program.cs
+++ Multidimensional Arrays/2.SquaresInMatrix/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _2.SquaresInMatrix
 {
@@ -10,7 +7,37 @@ namespace _2.SquaresInMatrix
     {
         static void Main(string[] args)
         {
+            int[] matrixArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = matrixArgs[0];
+            int cols = matrixArgs[1];
 
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] colArgs = Console.ReadLine().Split();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = colArgs[col];
+                }
+            }
+
+            int squaresCount = 0;
+            int squareSize = 2;
+
+            for (int row = 0; row < rows - squareSize + 1; row++)
+            {
+                for (int col = 0; col < cols - squareSize + 1; col++)
+                {
+                    if(matrix[row, col] == matrix[row, col+1] && matrix[row+1, col] == matrix[row+1, col+1] && matrix[row, col] == matrix[row+1, col+1])
+                    {
+                        squaresCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(squaresCount);
         }
     }
 }
