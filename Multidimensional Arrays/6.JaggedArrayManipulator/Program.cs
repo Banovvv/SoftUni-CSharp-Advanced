b9cac3f13efc20de2577234acbf0ad215822df48
Banovvv <ivan_banov@abv.bg>
2022-04-11 18:46:16 +0300

    6.Jagged Array Manipulator

---
+++ Multidimensional Arrays/6.JaggedArrayManipulator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace _6.JaggedArrayManipulator
+{
+    internal class Program
+    {
+        // 93/100
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            double[][] arrays = new double[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int[] colArgs = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                arrays[i] = new double[colArgs.Length];
+
+                for (int j = 0; j < colArgs.Length; j++)
+                {
+                    arrays[i][j] = colArgs[j];
+                }
+            }
+
+            // Modify based on adjesent row length
+            for (int i = 0; i < rows - 1; i++)
+            {
+                if (arrays[i].Length == arrays[i + 1].Length)
+                {
+                    for (int j = 0; j < arrays[i].Length; j++)
+                    {
+                        arrays[i][j] *= 2;
+                        arrays[i + 1][j] *= 2;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < arrays[i].Length; j++)
+                    {
+                        arrays[i][j] /= 2;
+                    }
+
+                    for (int j = 0; j < arrays[i + 1].Length; j++)
+                    {
+                        arrays[i + 1][j] /= 2;
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] commandArgs = command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string action = commandArgs[0];
+
+                int row = int.Parse(commandArgs[1]);
+                int col = int.Parse(commandArgs[2]);
+                int value = int.Parse(commandArgs[3]);
+
+                if (row < 0 || row > rows || col < 0 || col > arrays[row].Length)
+                {
+                    break;
+                }
+                else
+                {
+                    if (action == "Add")
+                    {
+                        arrays[row][col] += value;
+                    }
+                    else if (action == "Subtract")
+                    {
+                        arrays[row][col] -= value;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            // Print the jagged array
+            foreach (var array in arrays)
+            {
+                Console.WriteLine(string.Join(" ", array));
+            }
+        }
+    }
+}
