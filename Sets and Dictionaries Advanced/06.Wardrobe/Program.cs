0160e062e98dfc7658c72f37c178b4f4aedd9e63
Banovvv <ivan_banov@abv.bg>
2022-04-19 17:32:21 +0300

    06. Wardrobe

---
+++ Sets and Dictionaries Advanced/06.Wardrobe/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Wardrobe
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numberOfLine = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < numberOfLine; i++)
+            {
+                string[] lineArgs = Console.ReadLine().Split(new string[] { " -> " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string color = lineArgs[0];
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+
+                var clothes = lineArgs[1].Split(',');
+
+                foreach (var clothing in clothes)
+                {
+                    if (!wardrobe[color].ContainsKey(clothing))
+                    {
+                        wardrobe[color].Add(clothing, 0);
+                    }
+
+                    wardrobe[color][clothing]++;
+                }
+            }
+
+            string[] searchArgs = Console.ReadLine().Split();
+
+            string searchColor = searchArgs[0];
+            string searchClothing = searchArgs[1];
+
+
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine($"{color.Key} clothes:");
+
+                foreach (var clothing in color.Value)
+                {
+                    if (color.Key == searchColor && clothing.Key == searchClothing)
+                    {
+                        Console.WriteLine($"* {clothing.Key} - {clothing.Value} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothing.Key} - {clothing.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
