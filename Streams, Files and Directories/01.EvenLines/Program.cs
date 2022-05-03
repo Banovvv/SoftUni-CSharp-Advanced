469573374b9229758f7ec6cfea2f6fdb5cf65bc9
Banovvv <ivan_banov@abv.bg>
2022-05-03 11:24:24 +0300

    Updated project skeleton

--- Streams, Files and Directories/01.EvenLines/Program.cs
+++ Streams, Files and Directories/01.EvenLines/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _01.EvenLines
 {
-    internal class Program
+    internal class EvenLines
     {
         static void Main(string[] args)
         {
+            string inputFilePath = @"..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StreamReader sr = new StreamReader(inputFilePath);
+            string contents = sr.ReadToEnd();
+
+            string[] lines = contents.Split(new string[] { @"\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+
+            return sr.ReadToEnd();
+        }
+        private static string ReverseWords(string replacedSymbols)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string ReplaceSymbols(string line)
+        {
+            throw new NotImplementedException();
         }
     }
 }
