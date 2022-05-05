41d3f89e786b1aea23c612875542ff4dfb126089
Banovvv <ivan_banov@abv.bg>
2022-05-05 13:59:24 +0300

    03. Copy Binary File

--- Streams, Files and Directories/03.CopyBinaryFile/Program.cs
+++ Streams, Files and Directories/03.CopyBinaryFile/Program.cs
@@ -1,15 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace _03.CopyBinaryFile
 {
-    internal class Program
+    public class CopyBinaryFile
     {
         static void Main(string[] args)
         {
+            string inputPath = @"..\..\copyMe.png";
+            string outputPath = @"..\..\copyMe-copy.png";
+
+            CopyFile(inputPath, outputPath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (FileStream reader = new FileStream(inputFilePath, FileMode.Open))
+            {
+                byte[] buffer = new byte[reader.Length];
+
+                reader.Read(buffer, 0, buffer.Length);
+
+                using (FileStream writer = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    writer.Write(buffer, 0, buffer.Length);
+                }
+            }
         }
     }
 }
