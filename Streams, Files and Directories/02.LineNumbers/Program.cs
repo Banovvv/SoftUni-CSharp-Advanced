68dd450ddb3840724e86d11c49604e2e21aa2df3
Banovvv <ivan_banov@abv.bg>
2022-05-05 13:36:18 +0300

    Solution for 02. Line Numbers

--- Streams, Files and Directories/02.LineNumbers/Program.cs
+++ Streams, Files and Directories/02.LineNumbers/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _02.LineNumbers
 {
-    internal class Program
+    internal class LineNumbers
     {
         static void Main(string[] args)
         {
+            string inputPath = @"..\..\text.txt";
+            string outputPath = @"..\..\output.txt";
+
+            ProcessLines(inputPath, outputPath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] inputLines = File.ReadAllLines(inputFilePath);
+            string[] outputLines = new string[inputLines.Length];
+
+            for (int i = 1; i <= inputLines.Length; i++)
+            {
+                int lettersCount = inputLines[i - 1].Where(x => char.IsLetter(x)).Count();
+                int puncuationCount = inputLines[i - 1].Where(x => char.IsPunctuation(x)).Count();
+
+                outputLines[i - 1] = $"Line {i}: {inputLines[i - 1]} ({lettersCount})({puncuationCount})";
+            }
+
+            File.WriteAllLines(outputFilePath, outputLines);
         }
     }
 }
