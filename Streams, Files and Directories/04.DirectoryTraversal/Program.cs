bc45e8bb1a909e0191e39fa89fc9d77b8013a334
Banovvv <ivan_banov@abv.bg>
2022-05-12 09:15:00 +0300

    Skeleton

--- Streams, Files and Directories/04.DirectoryTraversal/Program.cs
+++ Streams, Files and Directories/04.DirectoryTraversal/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace _04.DirectoryTraversal
 {
-    internal class Program
+    public class DirectoryTraversal
     {
         static void Main(string[] args)
         {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
         }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
