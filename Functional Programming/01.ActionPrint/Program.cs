5f519d6f7147ef107f7be35bf29db33f167cf0c0
Banovvv <ivan_banov@abv.bg>
2022-05-13 10:44:32 +0300

    01.Action Print

---
+++ Functional Programming/01.ActionPrint/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ActionPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            Action<List<string>> action = PrintList;
+
+            action(words);
+        }
+
+        static void PrintList(List<string> words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
