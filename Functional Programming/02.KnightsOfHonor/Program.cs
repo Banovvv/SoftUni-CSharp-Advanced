71d1214880605634ce871f8bcc2f815d3edc12b6
Banovvv <ivan_banov@abv.bg>
2022-05-13 10:48:02 +0300

    02.Knights Of Honor

---
+++ Functional Programming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            Action<List<string>> action = PrintNames;
+
+            action(names);
+        }
+        static void PrintNames(List<string> names)
+        {
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Sir {name}");
+            }
+        }
+    }
+}
