2765c7bcf7cb9fa85f67d891635b0468293fa087
Banovvv <ivan_banov@abv.bg>
2022-05-13 10:55:59 +0300

    03. Custom Min Function

---
+++ Functional Programming/03.CustomMinFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Func<int[], int> func = FindMin;
+
+            Console.WriteLine(func(numbers));
+        }
+
+        static int FindMin(int[] numbers)
+        {
+            return numbers.Min(x => x);
+        }
+    }
+}
