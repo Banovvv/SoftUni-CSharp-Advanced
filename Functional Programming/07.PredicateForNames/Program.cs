84f7bdc9e1a3e9279e40661c6904c7b3ea88510b
Banovvv <ivan_banov@abv.bg>
2022-05-13 15:25:28 +0300

    07. Predicate For Names

---
+++ Functional Programming/07.PredicateForNames/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Predicate<string> predicate = x => x.Length <= length;
+            Action<List<string>, Predicate<string>> func = CheckLength;
+
+            func(names, predicate);
+        }
+
+        static void CheckLength(List<string> names, Predicate<string> predicate)
+        {
+
+            foreach (string name in names)
+            {
+                if (predicate(name))
+                {
+                    Console.WriteLine(name);
+                }
+            }
+        }
+    }
+}
