1029b790e4ddf102b9e63e306b5c1da102b63008
Banovvv <ivan_banov@abv.bg>
2022-05-16 17:55:39 +0300

    11. TriFunction

---
+++ Functional Programming/11.TriFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11.TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetCharSum = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split().ToList();
+
+            Func<string, int, bool> lengthFunc = CheckLength;
+            Func<Func<string, int, bool>, List<string>, int, string> nameFunc = GetName;
+
+            Console.WriteLine(nameFunc(lengthFunc, names, targetCharSum));
+        }
+
+        private static string GetName(Func<string, int, bool> lengthFunc, List<string> names, int targetCharSum) => names.Where(x => lengthFunc(x, targetCharSum)).First();
+
+        private static bool CheckLength(string word, int targetCharSum)
+        {
+            int charSum = 0;
+
+            foreach (char letter in word)
+            {
+                charSum += letter;
+            }
+
+            return charSum >= targetCharSum;
+        }
+    }
+}
