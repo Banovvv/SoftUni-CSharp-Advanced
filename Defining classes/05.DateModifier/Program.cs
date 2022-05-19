f45efd724f362f28d9dc474796fa451fec854e1e
Banovvv <ivan_banov@abv.bg>
2022-05-19 20:11:01 +0300

    5.DateModifier

---
+++ Defining classes/05.DateModifier/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05.DateModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+
+            DateModifier modifier = new DateModifier(dateOne, dateTwo);
+
+            Console.WriteLine(modifier.DateDifference());
+        }
+    }
+
+    public class DateModifier
+    {
+        public DateModifier(string dateOne, string dateTwo)
+        {
+            string[] dateOneArgs = dateOne.Split();
+            string[] dateTwoArgs = dateTwo.Split();
+
+            DateOne = new DateTime(int.Parse(dateOneArgs[0]), int.Parse(dateOneArgs[1]), int.Parse(dateOneArgs[2]));
+            DateTwo = new DateTime(int.Parse(dateTwoArgs[0]), int.Parse(dateTwoArgs[1]), int.Parse(dateTwoArgs[2]));
+        }
+
+        public DateTime DateOne { get; set; }
+        public DateTime DateTwo { get; set; }
+
+        public int DateDifference()
+        {
+            if(DateOne > DateTwo)
+            {
+                return (DateOne - DateTwo).Days;
+            }
+            else
+            {
+                return (DateTwo - DateOne).Days;
+            }
+        }
+    }
+}
