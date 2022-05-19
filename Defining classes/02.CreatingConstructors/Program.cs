2d9dc91771f3bcde0292699d2002b668c5357100
Banovvv <ivan_banov@abv.bg>
2022-05-19 18:45:42 +0300

    02.CreatingConstructors

---
+++ Defining classes/02.CreatingConstructors/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class Person
+    {
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
