c4a12c7f00815d5fd0ba62270f78eb673c925694
Banovvv <ivan_banov@abv.bg>
2022-05-19 19:58:42 +0300

    04.OpinionPoll

---
+++ Defining classes/04.OpinionPoll/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int numberOfMembers = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < numberOfMembers; i++)
+            {
+                string[] memberArgs = Console.ReadLine().Split();
+
+                string memberName = memberArgs[0];
+                int memberAge = int.Parse(memberArgs[1]);
+
+                Person member = new Person(memberName, memberAge);
+
+                family.AddMember(member);
+            }
+
+            List<Person> olderThanThirty = family.Members.Where(x => x.Age > 30).OrderBy(x=>x.Name).ToList();
+
+            foreach(var person in olderThanThirty)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+
+    public class Person
+    {
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Family
+    {
+        public Family()
+        {
+            Members = new List<Person>();
+        }
+
+        public List<Person> Members { get; set; }
+
+        public void AddMember(Person member)
+        {
+            Members.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return Members.Where(x => x.Age == Members.Max(x => x.Age)).FirstOrDefault();
+        }
+    }
+}
