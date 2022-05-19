ad1c81e97d1830c7191e649502f2ffc9f9c9e150
Banovvv <ivan_banov@abv.bg>
2022-05-19 22:33:25 +0300

    09.PokemonTrainer

---
+++ Defining classes/09.PokemonTrainer/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.PokemonTrainer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            #region Trainers
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Tournament")
+                {
+                    break;
+                }
+
+                string[] commandArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = commandArgs[0];
+                string pokemonName = commandArgs[1];
+                string pokemonElement = commandArgs[2];
+                int pokemonHealth = int.Parse(commandArgs[3]);
+
+                Trainer trainer = trainers.Where(x => x.Name == trainerName).FirstOrDefault();
+                Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                if (trainer == null)
+                {
+                    trainers.Add(new Trainer(trainerName, pokemon));
+                }
+                else
+                {
+                    trainer.Pokemons.Add(pokemon);
+                }
+            }
+            #endregion
+
+            #region Tournament
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+                PlayOutTurn(trainers, command);
+            }
+            #endregion
+
+            foreach (Trainer trainer in trainers.OrderByDescending(x => x.Badges))
+            {
+                Console.WriteLine(trainer);
+            }
+        }
+
+        static void PlayOutTurn(List<Trainer> trainers, string elementType)
+        {
+            foreach (Trainer trainer in trainers)
+            {
+                UpdateTrainer(trainer, elementType);
+            }
+
+        }
+
+        static void UpdateTrainer(Trainer trainer, string elementType)
+        {
+            if (trainer.Pokemons.FirstOrDefault(x => x.Element == elementType) != null)
+            {
+                trainer.Badges++;
+            }
+            else
+            {
+                foreach (Pokemon pokemon in trainer.Pokemons)
+                {
+                    pokemon.Health -= 10;
+                }
+            }
+
+            RemoveDeadPokemons(trainer);
+        }
+        static void RemoveDeadPokemons(Trainer trainer) => trainer.Pokemons = trainer.Pokemons.Where(x => x.Health > 0).ToList();
+    }
+}
---
+++ Defining classes/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace _09.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name, Pokemon pokemon)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemons.Add(pokemon);
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; } = new List<Pokemon>();
+
+        public override string ToString()
+        {
+            return $"{Name} {Badges} {Pokemons.Count}";
+        }
+    }
+}
