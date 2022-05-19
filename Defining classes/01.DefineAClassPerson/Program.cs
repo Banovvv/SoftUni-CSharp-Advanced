e75a5698a699d56f6c1531277e860dd80eba23b0
Banovvv <ivan_banov@abv.bg>
2022-05-19 17:30:16 +0300

    01.DefineAClassPerson

---
+++ Defining classes/01.DefineAClassPerson/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Peter";
+            person.Age = 20;
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
