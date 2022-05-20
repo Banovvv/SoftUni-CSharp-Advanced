a535050459c05bed7243914512ad3b4f725046b7
Banovvv <ivan_banov@abv.bg>
2022-05-20 14:14:35 +0300

    Added tests

--- Advanced/Generics/09.CustomLinkedList/StartUp.cs
+++ Advanced/Generics/09.CustomLinkedList/StartUp.cs
@@ -6,7 +6,28 @@ namespace CustomDoublyLinkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DoublyLinkedList customList = new DoublyLinkedList();
+
+            customList.AddFirst(3);
+            customList.AddLast(4);
+            Console.WriteLine(string.Join("-", customList.ToArray()));
+            // 3-4
+
+            customList.AddFirst(2);
+            customList.AddFirst(1);
+            customList.AddFirst(0);
+            Console.WriteLine(string.Join("-", customList.ToArray()));
+            // 0-1-2-3-4
+
+            customList.RemoveFirst();
+            customList.RemoveLast();
+            Console.WriteLine(string.Join("-", customList.ToArray()));
+            // 1-2-3
+
+            customList.ForEach(x => Console.WriteLine(x));
+            //1
+            //2
+            //3
         }
     }
 }
