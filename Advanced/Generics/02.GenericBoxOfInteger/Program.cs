941c574f404911a5997827ca503a6d7e1a298cbf
Banovvv <ivan_banov@abv.bg>
2022-05-20 18:14:25 +0300

    02.GenericBoxOfInteger

---
+++ Advanced/Generics/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.GenericBoxOfInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int value = int.Parse(Console.ReadLine());
+
+                var box = new Box<int>(value);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
