374695c859dcb019b911d0f9bfc17055891f38a3
Banovvv <ivan_banov@abv.bg>
2022-05-20 18:04:16 +0300

    01.GenericBoxOfString

---
+++ Advanced/Generics/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>("TEST");
+            Console.WriteLine(box);
+        }
+    }
+}
