c53977350e08098435180a39034a70638802487d
Banovvv <ivan_banov@abv.bg>
2022-05-23 14:32:56 +0300

    03.GenericSwapMethodString

---
+++ Advanced/Generics/03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Box<T>
+    {
+        public Box()
+        {
+
+        }
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString() => $"{typeof(T)}: {Value}";        
+    }
+}
---
+++ Advanced/Generics/03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            List<Box<string>> boxes = new List<Box<string>>();   
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string value = Console.ReadLine();
+
+                var box = new Box<string>(value);
+
+                boxes.Add(box);
+            }
+
+            int[] indices = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Swap(boxes, indices);
+
+            foreach (Box<string> box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+
+        static void Swap<T>(List<T> list, int[] indices)
+        {
+            var tempItem = list[indices[0]];
+            list[indices[0]] = list[indices[1]];
+            list[indices[1]] = tempItem;
+        }
+    }
+}
