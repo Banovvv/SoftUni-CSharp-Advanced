d4ae2ca9257c9022fe21cd045dad967cdd283f24
Banovvv <ivan_banov@abv.bg>
2022-05-24 18:59:52 +0300

    07.Tuple

---
+++ Advanced/Generics/07.Tuple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] lineOne = Console.ReadLine().Split();
+            Tuple<string, string> nameAndAddress = new Tuple<string, string>(lineOne[0] + " " + lineOne[1], lineOne[2]);
+
+            string[] lineTwo = Console.ReadLine().Split();
+            Tuple<string, int> nameAndLiters = new Tuple<string, int>(lineTwo[0], int.Parse(lineTwo[1]));
+
+            string[] lineThree = Console.ReadLine().Split();
+            Tuple<int, double> intAndDouble = new Tuple<int, double>(int.Parse(lineThree[0]), double.Parse(lineThree[1]));
+
+            Console.WriteLine(nameAndAddress);
+            Console.WriteLine(nameAndLiters);
+            Console.WriteLine(intAndDouble);
+        }
+    }
+}
