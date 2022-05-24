9c2c9649fe7eaeb80302ca0f03abd91102a7a36a
Banovvv <ivan_banov@abv.bg>
2022-05-24 18:40:40 +0300

    05.GenericCountMethodString

--- Advanced/Generics/05.GenericCountMethodString/Program.cs
+++ Advanced/Generics/05.GenericCountMethodString/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05.GenericCountMethodString
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfLines = int.Parse(Console.ReadLine());
+            List<string> lines = new List<string>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string value = Console.ReadLine();
+
+                lines.Add(value);
+            }
+
+            string compareTo = Console.ReadLine();
+
+            Box<string> box = new Box<string>(lines);
+
+            Console.WriteLine(box.CountEqualElements(compareTo));
         }
     }
 }
