42ac90fcf273653b57f6b3d19441196179749110
Banovvv <ivan_banov@abv.bg>
2022-05-24 19:10:40 +0300

    08.Threeuple

---
+++ Advanced/Generics/08.Threeuple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08.Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lineOne = Console.ReadLine().Split();
+            var namesAddressTown = new Threeuple<string, string, string>(lineOne[0] + " " + lineOne[1], lineOne[2], lineOne[3]);
+
+            string[] lineTwo = Console.ReadLine().Split();
+            var nameBeerDrunk = new Threeuple<string, int, bool>(lineTwo[0], int.Parse(lineTwo[1]), lineTwo[2] == "drunk" ? true : false);
+
+            string[] lineThree = Console.ReadLine().Split();
+            var nameBalanceBank = new Threeuple<string, double, string>(lineThree[0], double.Parse(lineThree[1]), lineThree[2]);
+
+            Console.WriteLine(namesAddressTown);
+            Console.WriteLine(nameBeerDrunk);
+            Console.WriteLine(nameBalanceBank);
+        }
+    }
+}
