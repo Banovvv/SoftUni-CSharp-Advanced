2a277e09113ae8976ca85f748ef101f1afeac758
Banovvv <ivan_banov@abv.bg>
2022-05-24 18:48:28 +0300

    06.GenericCountMethodDouble

---
+++ Advanced/Generics/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box(List<T> values)
+        {
+            Values = values;
+        }
+
+        public static List<T> Values { get; set; }
+
+        public int CountEqualElements(T element)
+        {
+            int count = 0;
+
+            foreach (T value in Values)
+            {
+                if (value.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Advanced/Generics/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            List<double> lines = new List<double>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                double value = double.Parse(Console.ReadLine());
+
+                lines.Add(value);
+            }
+
+            double compareTo = double.Parse(Console.ReadLine());
+
+            Box<double> box = new Box<double>(lines);
+
+            Console.WriteLine(box.CountEqualElements(compareTo));
+        }
+    }
+}
