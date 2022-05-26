cdb17732bd5d38ca15486e60c09449429d4e84d5
Banovvv <ivan_banov@abv.bg>
2022-05-26 20:52:38 +0300

    01.ListyIterator

---
+++ Advanced/Iterators and Comparators/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private int index = 0;
+
+        public ListyIterator(List<T> elements)
+        {
+            Elements = elements;
+        }
+
+        public List<T> Elements { get; set; } = new List<T>();
+
+        public bool Move()
+        {
+            if (index + 1 < Elements.Count)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            return index + 1 < Elements.Count;
+        }
+        public void Print()
+        {
+            if (!Elements.Any())
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(Elements[index]);
+            }
+        }
+    }
+}
---
+++ Advanced/Iterators and Comparators/01.ListyIterator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> createCommand = Console.ReadLine().Split().ToList();
+
+            ListyIterator<string> customList = new ListyIterator<string>(createCommand.Skip(1).ToList());
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                if (command == "HasNext")
+                {
+                    Console.WriteLine(customList.HasNext());
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(customList.Move());
+                }
+                else if (command == "Print")
+                {
+                    customList.Print();
+                }
+            }
+        }
+    }
+}
