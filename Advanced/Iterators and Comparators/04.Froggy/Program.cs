6663bdb63bd538c3a91552ed5ff71b96467172f5
Banovvv <ivan_banov@abv.bg>
2022-05-27 20:54:39 +0300

    04. Froggy

---
+++ Advanced/Iterators and Comparators/04.Froggy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stones = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+
+            var lake = new Lake(stones);
+
+            var stoneOrder = new List<int>();
+
+            foreach (var stone in lake)
+            {
+                stoneOrder.Add(stone);
+            }
+
+            Console.WriteLine(string.Join(", ", stoneOrder));
+        }
+    }
+}
