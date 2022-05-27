497c0c2e663773c204b1f1f356ab10fbe6a4aee9
Banovvv <ivan_banov@abv.bg>
2022-05-27 13:30:52 +0300

    02. Collection

---
+++ Advanced/Iterators and Comparators/02.Collection/ListyIterator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private int index = 0;
+
+        public ListyIterator(List<T> elements)
+        {
+            Elements = elements;
+        }
+
+        public List<T> Elements { get; set; } = new List<T>();
+
+        public bool Move()
+        {
+            if (index + 1 < Elements.Count)
+            {
+                index++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext()
+        {
+            return index + 1 < Elements.Count;
+        }
+        public void Print()
+        {
+            if (!Elements.Any())
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+            else
+            {
+                Console.WriteLine(Elements[index]);
+            }
+        }
+
+        public void PrintAll()
+        {
+            foreach (T element in Elements)
+            {
+                Console.Write($"{element} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Elements.Count; i++)
+            {
+                yield return Elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Advanced/Iterators and Comparators/02.Collection/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Collection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> createCommand = Console.ReadLine().Split().ToList();
+
+            ListyIterator<string> customList = new ListyIterator<string>(createCommand.Skip(1).ToList());
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                if (command == "HasNext")
+                {
+                    Console.WriteLine(customList.HasNext());
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(customList.Move());
+                }
+                else if (command == "Print")
+                {
+                    customList.Print();
+                }
+                else if (command == "PrintAll")
+                {
+                    customList.PrintAll();
+                }
+            }
+        }
+    }
+}
