a003a9ef77b5e93cfdec6f76fc0b8b83581ae5cc
Banovvv <ivan_banov@abv.bg>
2022-05-27 21:18:00 +0300

    05. Comparing Objects

---
+++ Advanced/Iterators and Comparators/05.ComparingObjects/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            if (Name.CompareTo(other.Name) > 0)
+            {
+                return 1;
+            }
+            else if (Name.CompareTo(other.Name) < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                if (Age > other.Age)
+                {
+                    return 1;
+                }
+                else if (Age < other.Age)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (Town.CompareTo(other.Town) > 0)
+                    {
+                        return 1;
+                    }
+                    else if (Town.CompareTo(other.Town) < 0)
+                    {
+                        return -1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/Iterators and Comparators/05.ComparingObjects/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.ComparingObjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            while (true)
+            {
+                string[] personInfo = Console.ReadLine().Split();
+
+                if (personInfo.First() == "END")
+                {
+                    break;
+                }
+
+                people.Add(new Person(personInfo[0], int.Parse(personInfo[1]), personInfo[2]));
+            }
+
+            int index = int.Parse(Console.ReadLine());
+
+            Person personToCompareWith = people[index - 1];
+
+            int matches = 0;
+
+            foreach(Person person in people.Where(x=>x != personToCompareWith))
+            {
+                if(personToCompareWith.CompareTo(person) == 0)
+                {
+                    matches++;
+                }
+            }
+
+            if (matches > 0)
+            {
+                Console.WriteLine($"{matches + 1} {people.Count - (matches + 1)} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
