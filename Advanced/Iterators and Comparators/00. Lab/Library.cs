ba9513d15c98086b617dc9d5ceb821a88ffc868a
Banovvv <ivan_banov@abv.bg>
2022-05-27 09:03:28 +0300

    First Lab problem - 01. Library

---
+++ Advanced/Iterators and Comparators/00. Lab/Library.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            Books = new List<Book>(books);
+        }
+
+        public List<Book> Books { get; set; }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
