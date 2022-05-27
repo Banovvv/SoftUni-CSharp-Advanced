386bbc8c9462149c04db36023cb1defd972f895b
Banovvv <ivan_banov@abv.bg>
2022-05-27 13:50:42 +0300

    03. Stack

---
+++ Advanced/Iterators and Comparators/03.Stack/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Stack
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> createCommand = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Stack<string> customStack = new Stack<string>(createCommand.Skip(1).ToArray());
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                if (command.Contains("Push"))
+                {
+                    customStack.Push(command.Split().Last());
+                }
+                else if (command == "Pop")
+                {
+                    customStack.Pop();
+                }
+            }
+
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Advanced/Iterators and Comparators/03.Stack/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        public Stack(params T[] elements)
+        {
+            Elements = new List<T>(elements);
+        }
+
+        public List<T> Elements { get; set; }
+
+        public void Push(T element)
+        {
+            Elements.Add(element);
+        }
+
+        public T Pop()
+        {
+            if (Elements.Count == 0)
+            {
+                Console.WriteLine("No elements");
+                return default(T);
+            }
+
+            T element = Elements.Last();
+            Elements.Remove(element);
+
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Elements.Count - 1; i >= 0; i--)
+            {
+                yield return Elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
