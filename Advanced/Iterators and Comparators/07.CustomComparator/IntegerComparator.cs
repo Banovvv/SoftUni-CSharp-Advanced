b5547f21f1db609594603eddebf3fc19b6d9b56d
Banovvv <ivan_banov@abv.bg>
2022-05-28 14:17:04 +0300

    07. Custom Comparator

---
+++ Advanced/Iterators and Comparators/07.CustomComparator/IntegerComparator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _07.CustomComparator
+{
+    public class IntegerComparator : IComparer<int>
+    {
+        public int Compare([AllowNull] int x, [AllowNull] int y)
+        {
+            if (x % 2 != 0 && y % 2 == 0)
+            {
+                return 1;
+            }
+            else if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return x.CompareTo(y);
+            }
+        }
+    }
+}
---
+++ Advanced/Iterators and Comparators/07.CustomComparator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.CustomComparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            array.Sort(new IntegerComparator());
+
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
