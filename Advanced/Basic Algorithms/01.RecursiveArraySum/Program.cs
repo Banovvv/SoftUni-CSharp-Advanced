ba659522052c8600f2358f857709cc48f3fe5eb3
Banovvv <ivan_banov@abv.bg>
2022-05-29 15:59:30 +0300

    01. Recursive Array Sum

---
+++ Advanced/Basic Algorithms/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(Sum(array, 0));
+        }
+
+        static int Sum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            int result = Sum(array, index + 1);
+
+            return array[index] + result;
+        }
+    }
+}
