e877f9512cf81b65a4cb5e46ea42791a38a344b4
Banovvv <ivan_banov@abv.bg>
2022-05-30 09:58:08 +0300

    02. Recursive Factorial

---
+++ Advanced/Basic Algorithms/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02.RecursiveFactorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(FactorialSum(n));
+        }
+
+        static long FactorialSum(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return n * FactorialSum(n - 1);
+        }
+    }
+}
