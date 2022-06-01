5b9e79523239064305296279066796818e99b3ff
Banovvv <ivan_banov@abv.bg>
2022-06-01 12:07:14 +0300

    04. Set Cover - Greedy

---
+++ Advanced/Basic Algorithms/04.SetCover/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetCover
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<int> universe = Console.ReadLine().Split(", ").Select(int.Parse).ToList();
+            List<int[]> sets = new List<int[]>();
+
+            int numberOfSets = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfSets; i++)
+            {
+                int[] currentSet = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                sets.Add(currentSet);
+            }
+
+            List<int[]> setsToTake = ChooseSets(sets, universe);
+
+            Console.WriteLine($"Sets to take ({setsToTake.Count}):");
+            foreach (int[] set in setsToTake)
+            {
+                Console.WriteLine($"{{ {string.Join(", ", set)} }}");
+            }
+        }
+
+        public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+        {
+            List<int[]> setsToTake = new List<int[]>();
+
+            while (universe.Count > 0 && sets.Count > 0)
+            {
+                var mostmatches = sets.OrderByDescending(x => x.Count(universe.Contains)).First();
+
+                setsToTake.Add(mostmatches);
+                sets.Remove(mostmatches);
+
+                List<int> elementsToRemoveFromUniverse = new List<int>();
+
+                foreach (var element in universe)
+                {
+                    if (mostmatches.Contains(element))
+                    {
+                        elementsToRemoveFromUniverse.Add(element);
+                    }
+                }
+
+                foreach (var element in elementsToRemoveFromUniverse)
+                {
+                    universe.Remove(element);
+                }
+            }
+
+            return setsToTake;
+        }
+    }
+}
