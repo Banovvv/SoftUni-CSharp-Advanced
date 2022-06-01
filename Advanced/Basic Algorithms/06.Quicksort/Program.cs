f4aed5c16c4e91eed6e7f2c1be8204ae1859898b
Banovvv <ivan_banov@abv.bg>
2022-06-01 20:16:36 +0300

    Not properly working, will update later

---
+++ Advanced/Basic Algorithms/06.Quicksort/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.Quicksort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            QuickSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+        //static void QuickSort(int[] array)
+        //{
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        int low = i;
+
+        //        for (int j = array.Length - 1; j > i; j--)
+        //        {
+        //            if (array[j] < array[i])
+        //            {
+        //                low = j;
+        //            }
+        //        }
+
+        //        SwapValues(array, low, i);
+        //    }
+        //}
+        static void QuickSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int low = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[i])
+                    {
+                        low = j;
+                    }
+                }
+
+                SwapValues(array, low, i);
+            }
+        }
+
+        static void SwapValues(int[] array, int low, int high)
+        {
+            int temp = array[low];
+            array[low] = array[high];
+            array[high] = temp;
+        }
+    }
+
+    public class QuickSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable<T>
+        {
+
+        }
+
+        private static void SwapValues<T>(T[] array, int low, int high)
+        {
+
+        }
+    }
+}
