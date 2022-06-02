39f8290e04626dc7197ac6180844db656c99d87f
Banovvv <ivan_banov@abv.bg>
2022-06-02 09:25:57 +0300

    Updated the code and introduced better logic

--- Advanced/Basic Algorithms/06.Quicksort/Program.cs
+++ Advanced/Basic Algorithms/06.Quicksort/Program.cs
@@ -9,48 +9,56 @@ namespace _06.Quicksort
         {
             int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            QuickSort(numbers);
+            QuickSort.Sort(numbers);
 
             Console.WriteLine(string.Join(" ", numbers));
-        }
-        static void QuickSort(int[] array)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                int low = i;
-                int minNumber = int.MaxValue;
-
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[j] < array[i] && array[j] < minNumber)
-                    {
-                        low = j;
-                        minNumber = array[j];
-                    }
-                }
-
-                SwapValues(array, low, i);
-            }
-        }
-
-        static void SwapValues(int[] array, int low, int high)
-        {
-            int temp = array[low];
-            array[low] = array[high];
-            array[high] = temp;
         }
     }
 
     public class QuickSort
     {
-        public static void Sort<T>(T[] array) where T : IComparable<T>
+        public static void Sort(int[] array)
         {
-
+            Sort(array, 0, array.Length - 1);
         }
 
-        private static void SwapValues<T>(T[] array, int low, int high)
+        private static void Sort(int[] array, int low, int high)
         {
+            int localPivot = 0;
 
+            if (low < high)
+            {
+                localPivot = Partition(array, low, high);
+                Sort(array, low, localPivot - 1);
+                Sort(array, localPivot + 1, high);
+            }
+        }
+
+        private static int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            Swap(array, i + 1, high);
+
+            return i + 1;
+        }
+
+        private static void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
         }
     }
 }
