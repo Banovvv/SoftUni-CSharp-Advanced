8f3aca3f633754708f13d8212372799ceda201ef
Banovvv <ivan_banov@abv.bg>
2022-06-06 14:31:52 +0300

    05. Restaurant

---
+++ OOP/Inheritance/05.Restaurant/Beverage/Hot/Coffee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, decimal price, double milliliters) : base(name, price, milliliters)
+        {
+        }
+
+        public double Caffeine { get; set; }
+        public double CoffeeMilliliters = 50;
+        public decimal CoffeePrice = 3.50M;
+
+    }
+}
---
+++ OOP/Inheritance/05.Restaurant/Food/Dessert/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double DefaultCakeGrams = 250;
+        private const double DefaultCakeCalories = 1000;
+        private const decimal DefaultCakePrice = 5m;
+
+        public Cake(string name) : base(name, DefaultCakePrice, DefaultCakeGrams, DefaultCakeCalories)
+        {
+        }
+    }
+}
