4ddcb7dc3164441353ef6d1566a01f075f9c155c
Banovvv <ivan_banov@abv.bg>
2022-06-12 15:04:25 +0300

    Define an Interface IPerson

---
+++ OOP/Interfaces and Abstraction/01.DefineАnInterfaceIPerson/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonInfo
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
