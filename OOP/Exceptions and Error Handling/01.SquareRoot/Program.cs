15c6ee6774569f2b119a37ce146b304d04f12c70
Banovvv <ivan_banov@abv.bg>
2022-06-17 10:32:47 +0300

    01. Square root

---
+++ OOP/Exceptions and Error Handling/01.SquareRoot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01._quareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (n < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
