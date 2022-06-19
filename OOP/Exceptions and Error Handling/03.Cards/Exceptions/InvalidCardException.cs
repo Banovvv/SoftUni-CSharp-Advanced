a5db4c99af2e92095e7ddbec17ac37c966caeec8
Banovvv <ivan_banov@abv.bg>
2022-06-19 09:58:25 +0300

    Exception done, working on Card class

---
+++ OOP/Exceptions and Error Handling/03.Cards/Exceptions/InvalidCardException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03.Cards.Exceptions
+{
+    public class InvalidCardException : Exception
+    {
+        private const string _message = "Invalid card!";
+
+        public InvalidCardException() : base(_message)
+        {
+
+        }
+    }
+}
---
+++ OOP/Exceptions and Error Handling/03.Cards/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Cards.Models
+{
+    public class Card
+    {
+        private readonly string[] _validFaces = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        public Card(string face, string suit)
+        {
+
+        }
+
+        public string Face { get; private set; }
+        public string Suit { get; private set; }
+
+        public override string ToString() => $"[{Face}{Suit}]";
+    }
+}
