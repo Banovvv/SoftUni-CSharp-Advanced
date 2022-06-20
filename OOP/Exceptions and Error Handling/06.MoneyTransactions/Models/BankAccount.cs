77379d49e41dabdbc31c5da97e35c8234ada029e
Banovvv <ivan_banov@abv.bg>
2022-06-20 18:45:36 +0300

    06. Money Transactions

--- OOP/Exceptions and Error Handling/06.MoneyTransactions/Models/BankAccount.cs
+++ OOP/Exceptions and Error Handling/06.MoneyTransactions/Models/BankAccount.cs
@@ -2,11 +2,13 @@
 {
     public class BankAccount
     {
-        public BankAccount(decimal balance)
+        public BankAccount(int id, decimal balance)
         {
+            ID = id;    
             Balance = balance;
         }
 
+        public int ID { get; set; }
         public decimal Balance { get; set; }
     }
 }
--- OOP/Exceptions and Error Handling/06.MoneyTransactions/Program.cs
+++ OOP/Exceptions and Error Handling/06.MoneyTransactions/Program.cs
@@ -1,6 +1,7 @@
 ﻿using _06.MoneyTransactions.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _06.MoneyTransactions
 {
@@ -14,9 +15,10 @@ namespace _06.MoneyTransactions
             foreach (string bankAccountArg in bankAccountArgs)
             {
                 string[] accountParams = bankAccountArg.Split('-', StringSplitOptions.RemoveEmptyEntries);
+                int id = int.Parse(accountParams[0]);
                 decimal amount = decimal.Parse(accountParams[1]);
 
-                bankAccounts.Add(new BankAccount(amount));
+                bankAccounts.Add(new BankAccount(id, amount));
             }
 
             while (true)
@@ -31,18 +33,18 @@ namespace _06.MoneyTransactions
                 string[] commandArgs = commandParams.Split();
 
                 string command = commandArgs[0];
-                int accountNumber = int.Parse(commandArgs[1]);
+                int id = int.Parse(commandArgs[1]);
                 decimal amount = decimal.Parse(commandArgs[2]);
 
                 try
                 {
                     if (command == "Deposit")
                     {
-                        Deposit(bankAccounts, accountNumber, amount);
+                        Deposit(bankAccounts, id, amount);
                     }
                     else if (command == "Withdraw")
                     {
-                        Withdraw(bankAccounts, accountNumber, amount);
+                        Withdraw(bankAccounts, id, amount);
                     }
                     else
                     {
@@ -60,33 +62,37 @@ namespace _06.MoneyTransactions
             }
         }
 
-        private static void Withdraw(IList<BankAccount> bankAccounts, int accountNumber, decimal amount)
+        private static void Withdraw(IList<BankAccount> bankAccounts, int id, decimal amount)
         {
-            if (accountNumber < 1 || accountNumber > bankAccounts.Count)
+            BankAccount currentAccount = bankAccounts.Where(x => x.ID == id).FirstOrDefault();
+
+            if (currentAccount == null)
             {
                 throw new ArgumentException("Invalid account!");
             }
 
-            if(bankAccounts[accountNumber - 1].Balance - amount < 0)
+            if (currentAccount.Balance - amount < 0)
             {
                 throw new ArgumentException("Insufficient balance!");
             }
 
-            bankAccounts[accountNumber - 1].Balance -= amount;
+            currentAccount.Balance -= amount;
 
-            Console.WriteLine($"Account {accountNumber} has new balance: {bankAccounts[accountNumber - 1].Balance}");
+            Console.WriteLine($"Account {id} has new balance: {currentAccount.Balance}");
         }
 
-        private static void Deposit(IList<BankAccount> bankAccounts, int accountNumber, decimal amount)
+        private static void Deposit(IList<BankAccount> bankAccounts, int id, decimal amount)
         {
-            if(accountNumber < 1 || accountNumber > bankAccounts.Count)
+            BankAccount currentAccount = bankAccounts.Where(x => x.ID == id).FirstOrDefault();
+
+            if (currentAccount == null)
             {
                 throw new ArgumentException("Invalid account!");
             }
 
-            bankAccounts[accountNumber - 1].Balance += amount;
+            currentAccount.Balance += amount;
 
-            Console.WriteLine($"Account {accountNumber} has new balance: {bankAccounts[accountNumber - 1].Balance}");
+            Console.WriteLine($"Account {id} has new balance: {currentAccount.Balance}");
         }
     }
 }
