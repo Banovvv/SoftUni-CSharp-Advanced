62e2936970d7720ecd362eea103540cfbf3db60b
Banovvv <ivan_banov@abv.bg>
2022-06-24 19:55:21 +0300

    01. Command Pattern

---
+++ OOP/Reflection and Attributes/01.CommandPattern/Core/Models/CommandInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core.Models
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        public string Read(string args)
+        {
+            var commandArgs = args.Split().ToArray();
+
+            var commandName = commandArgs[0];
+            var fullCommandName = commandName + "Command";
+
+            Type type = Assembly.GetCallingAssembly().GetTypes().Where(x => x.GetInterfaces().Any(x => x.Name == nameof(ICommand))).FirstOrDefault(x => x.Name == fullCommandName);
+
+            if (type == null)
+            {
+                throw new ArgumentException("Invalid command!");
+            }
+
+            ICommand command = Activator.CreateInstance(type) as ICommand;
+
+            var clearData = commandArgs.Skip(1).ToArray();
+
+            return command.Execute(clearData); ;
+        }
+    }
+}
---
+++ OOP/Reflection and Attributes/01.CommandPattern/Core/Models/Engine.cs
@@ -0,0 +1,27 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern.Core.Models
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                var commandResult = this.commandInterpreter.Read(input);
+
+                Console.WriteLine(commandResult);
+            }
+        }
+    }
+}
