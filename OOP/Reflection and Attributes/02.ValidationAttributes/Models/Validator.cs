a146678fe17942881df351423b1652582793d3ef
Banovvv <ivan_banov@abv.bg>
2022-06-24 20:17:58 +0300

    02. Validation Attributes

---
+++ OOP/Reflection and Attributes/02.ValidationAttributes/Models/Validator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Models
+{
+    public class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type type = obj.GetType();
+
+            PropertyInfo[] propertyInfos = type.GetProperties();
+
+            foreach (var propertyInfo in propertyInfos)
+            {
+                List<MyValidationAttribute> attributes = propertyInfo.GetCustomAttributes<MyValidationAttribute>().ToList();
+
+                var propertyValue = propertyInfo.GetValue(obj);
+
+                foreach (var attribute in attributes)
+                {
+                    if (attribute.IsValid(propertyValue))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
