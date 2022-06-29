c1a7ae81c5de9ce753dacb4882658cdf98c6b896
Banovvv <ivan_banov@abv.bg>
2022-06-29 16:28:25 +0300

    Skeleton

---
+++ OOP/Unit Testing/04. Fighting Arena/FightingArena.Tests/ArenaTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace FightingArena.Tests
+{
+    [TestFixture]
+    public class ArenaTests
+    {
+        //You should test all the constructors
+        //You should test all properties(getters and setters)
+        //You should test all the methods and validations inside the class}
+
+    }
+}
---
+++ OOP/Unit Testing/04. Fighting Arena/FightingArena.Tests/WarriorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace FightingArena.Tests
+{
+    [TestFixture]
+    public class WarriorTests
+    {
+        //You should test all the constructors
+        //You should test all properties(getters and setters)
+        //You should test all the methods and validations inside the class}
+
+    }
+}
