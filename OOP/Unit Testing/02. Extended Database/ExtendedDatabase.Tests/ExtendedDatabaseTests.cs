d15e31fa56128bc6f9d8ad7e979bdfcc9e44cd01
Banovvv <ivan_banov@abv.bg>
2022-06-29 12:27:53 +0300

    Tests complete

--- OOP/Unit Testing/02. Extended Database/ExtendedDatabase.Tests/ExtendedDatabaseTests.cs
+++ OOP/Unit Testing/02. Extended Database/ExtendedDatabase.Tests/ExtendedDatabaseTests.cs
@@ -35,8 +35,115 @@ namespace ExtendedDatabase.Tests
             Assert.Throws<InvalidOperationException>(() => database.Add(new Person(17, "Person17")), "Array's capacity must be exactly 16 integers!");
         }
 
+        [TestCase(1, "Person1")]
+        [TestCase(20, "Person20")]
+        public void AddMethodShouldThrowExceptionWhenUsernameAlreadyExists(long id, string username)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+            database.Add(new Person(id, username));
+
+            Assert.Throws<InvalidOperationException>(() => database.Add(new Person(id, username)), "There is already user with this username!");
+        }
+
+        [TestCase(1, "Person1")]
+        [TestCase(20, "Person20")]
+        public void AddMethodShouldThrowExceptionWhenIdAlreadyExists(long id, string username)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+            database.Add(new Person(id, username));
+
+            Assert.Throws<InvalidOperationException>(() => database.Add(new Person(id, username)), "There is already user with this Id!");
+        }
+
+        [Test]
+        public void RemoveShouldThrowExcetionWhenCountIsZero()
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            Assert.Throws<InvalidOperationException>(() => database.Remove());
+        }
+
+        [TestCase(1, 1)]
+        [TestCase(15, 5)]
+        public void RemoveShouldWorkWhenCountIsAboveZero(long count, int toRemove)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            for (int i = 0; i < count; i++)
+            {
+                database.Add(new Person(i, $"Person{i}"));
+            }
+
+            for (int i = 0; i < toRemove; i++)
+            {
+                database.Remove();
+            }
+
+            Assert.AreEqual(count - toRemove, database.Count);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void FindByUsernameShouldThrowExceptionForNullOrEmptyString(string username)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            Assert.Throws<ArgumentNullException>(() => database.FindByUsername(username), "Username parameter is null!");
+        }
+
+        [TestCase("NqmaMe")]
+        [TestCase("NotPresent")]
+        public void FindByUsernameShouldThrowExceptionWhenUsernameIsNotPresent(string username)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            Assert.Throws<InvalidOperationException>(() => database.FindByUsername(username), "No user is present by this username!");
+        }
+
+        [TestCase(1, "Person")]
+        public void FindByUsernameShouldWorkWhenUsernameIsPresent(long id, string username)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+            database.Add(new Person(id, username));
+
+            Assert.AreNotEqual(null, database.FindByUsername(username));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-99)]
+        [TestCase(-9999999999)]
+        public void FindByIdShouldThrowExceptionWhenIdIsBelowZero(long id)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => database.FindById(id), "Id should be a positive number!");
+        }
+
+        [TestCase(1)]
+        [TestCase(99)]
+        [TestCase(9999999999)]
+        public void FindByIdShouldThrowExceptionWhenIdIsNotPresent(long id)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            Assert.Throws<InvalidOperationException>(() => database.FindById(id), "No user is present by this ID!");
+        }
+
+        [TestCase(1)]
+        [TestCase(99)]
+        [TestCase(9999999999)]
+        public void FindByIdShouldWorkWhenIdIsPresent(long id)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            database.Add(new Person(id, $"Person{id}"));
+
+            Assert.AreNotEqual(null, database.FindById(id));
+        }
+
         [TestCase(20)]
         [TestCase(25)]
+        [TestCase(30)]
         public void ConstructorShouldThrowExceptionWhenItemsAreAboveSixteen(int count)
         {
             Person[] people = new Person[count];
@@ -47,6 +154,23 @@ namespace ExtendedDatabase.Tests
             }
 
             Assert.Throws<ArgumentException>(() => new ExtendedDatabase(people));
+        }
+
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(15)]
+        public void ConstructorShouldAddElementsWhileTheyArebelowSixteen(int count)
+        {
+            Person[] people = new Person[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                people[i] = new Person(i, $"Person{i}");
+            }
+
+            ExtendedDatabase database = new ExtendedDatabase(people);
+
+            Assert.AreEqual(count, database.Count);
         }
     }
 }
