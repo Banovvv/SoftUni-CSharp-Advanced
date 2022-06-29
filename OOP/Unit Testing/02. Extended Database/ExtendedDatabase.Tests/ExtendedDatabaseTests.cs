9b2972d8ab41a2cc16d7cc38659d4c7c0928d7c6
Banovvv <ivan_banov@abv.bg>
2022-06-29 10:43:44 +0300

    Expanding the tests

--- OOP/Unit Testing/02. Extended Database/ExtendedDatabase.Tests/ExtendedDatabaseTests.cs
+++ OOP/Unit Testing/02. Extended Database/ExtendedDatabase.Tests/ExtendedDatabaseTests.cs
@@ -6,6 +6,47 @@ namespace ExtendedDatabase.Tests
     [TestFixture]
     public class ExtendedDatabaseTests
     {
-        
+        [TestCase(3)]
+        [TestCase(12)]
+        [TestCase(16)]
+        [TestCase(0)]
+        public void AddMethodShouldAddNewElementsWhileCountIsLessThanSixteen(int count)
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            for (int i = 0; i < count; i++)
+            {
+                database.Add(new Person(i, $"Person{i}"));
+            }
+
+            Assert.AreEqual(count, database.Count);
+        }
+
+        [Test]
+        public void AddMethodShouldThrowExceptionWhenItemsAreAboveSixteen()
+        {
+            ExtendedDatabase database = new ExtendedDatabase();
+
+            for (int i = 0; i < 16; i++)
+            {
+                database.Add(new Person(i, $"Person{i}"));
+            }
+
+            Assert.Throws<InvalidOperationException>(() => database.Add(new Person(17, "Person17")), "Array's capacity must be exactly 16 integers!");
+        }
+
+        [TestCase(20)]
+        [TestCase(25)]
+        public void ConstructorShouldThrowExceptionWhenItemsAreAboveSixteen(int count)
+        {
+            Person[] people = new Person[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                people[i] = new Person(i, $"Person{i}");
+            }
+
+            Assert.Throws<ArgumentException>(() => new ExtendedDatabase(people));
+        }
     }
 }
--- OOP/Unit Testing/02. Extended Database/ExtendedDatabase.Tests/PersonTests.cs
+++ OOP/Unit Testing/02. Extended Database/ExtendedDatabase.Tests/PersonTests.cs
@@ -5,7 +5,9 @@ namespace ExtendedDatabase.Tests
     [TestFixture]
     public class PersonTests
     {
+        [TestCase(1, "Robot00")]
         [TestCase(1234124151, "Robot01")]
+        [TestCase(1234124151142, "Robot02")]
         public void ConstructorShouldWorkWithValidParameters(long id, string username)
         {
             var person = new Person(id, username);
