7818029930bb5cd21c4e16cd7eb471057b4f2ddc
Banovvv <ivan_banov@abv.bg>
2022-06-29 15:55:43 +0300

    Initial skeleton and tests

---
+++ OOP/Unit Testing/03. Car Manager/CarManager.Tests/CarTests.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using System;
+
+namespace CarManager.Tests
+{
+    [TestFixture]
+    public class CarTests
+    {
+        //You should test all the constructors
+        //You should test all properties(getters and setters)
+        //You should test all the methods and validations inside the class}
+
+        [TestCase("Lada", "Niva", 15.5, 80)]
+        [TestCase("Tesla", "Model X", 0.1, 0.1)]
+        public void ConstructorShouldWorkWithValidParameters(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            Car car = new Car(make, model, fuelConsumption, fuelCapacity);
+
+            Assert.AreEqual(make, car.Make);
+            Assert.AreEqual(model, car.Model);
+            Assert.AreEqual(fuelConsumption, car.FuelConsumption);
+            Assert.AreEqual(fuelCapacity, car.FuelCapacity);
+            Assert.AreEqual(0, car.FuelAmount);
+        }
+
+        [TestCase("", "Niva", 15.5, 80)]
+        [TestCase(null, "Model X", 0.1, 0.1)]
+        public void ConstructorShouldThrowExceptionWithInvalidMake(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            Assert.Throws<ArgumentException>(() => new Car(make, model, fuelConsumption, fuelCapacity), "Make cannot be null or empty!");
+        }
+
+        [TestCase("Lada", "", 15.5, 80)]
+        [TestCase("Tesla", null, 0.1, 0.1)]
+        public void ConstructorShouldThrowExceptionWithInvalidModel(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            Assert.Throws<ArgumentException>(() => new Car(make, model, fuelConsumption, fuelCapacity), "Model cannot be null or empty!");
+        }
+
+        [TestCase("Lada", "Niva", 0, 80)]
+        [TestCase("Tesla", "Model X", -1, 0.1)]
+        public void ConstructorShouldThrowExceptionWithInvalidFuelConsumption(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            Assert.Throws<ArgumentException>(() => new Car(make, model, fuelConsumption, fuelCapacity), "Fuel consumption cannot be zero or negative!");
+        }
+
+        [TestCase("Lada", "Niva", 15.5, 0)]
+        [TestCase("Tesla", "Model X", 0.1, -1)]
+        public void ConstructorShouldThrowExceptionWithInvalidFuelCapacity(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            Assert.Throws<ArgumentException>(() => new Car(make, model, fuelConsumption, fuelCapacity), "Fuel capacity cannot be zero or negative!");
+        }
+    }
+}
