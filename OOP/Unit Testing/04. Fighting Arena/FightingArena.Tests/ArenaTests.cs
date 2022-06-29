9a08d547f6d59ec2b6665d6842a055172ac11b69
Banovvv <ivan_banov@abv.bg>
2022-06-29 20:58:09 +0300

    Tests are done

--- OOP/Unit Testing/04. Fighting Arena/FightingArena.Tests/ArenaTests.cs
+++ OOP/Unit Testing/04. Fighting Arena/FightingArena.Tests/ArenaTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace FightingArena.Tests
 {
@@ -8,6 +9,78 @@ namespace FightingArena.Tests
         //You should test all the constructors
         //You should test all properties(getters and setters)
         //You should test all the methods and validations inside the class}
+        private Arena _arena;
 
+        [SetUp]
+        public void Initialize()
+        {
+
+            _arena = new Arena();
+        }
+        [Test]
+        public void ConstructorShouldWork()
+        {
+            Assert.True(_arena.Warriors != null);
+            Assert.AreEqual(0, _arena.Warriors.Count);
+        }
+
+        #region Enroll Tests
+        [Test]
+        public void EnrollShouldWorkWithValidparameters()
+        {
+            var _arena = new Arena();
+
+            _arena.Enroll(new Warrior("Pesho", 10, 30));
+            _arena.Enroll(new Warrior("Gosho", 10, 30));
+            _arena.Enroll(new Warrior("Tosho", 10, 30));
+
+            Assert.AreEqual(3, _arena.Count);
+            Assert.AreEqual(3, _arena.Warriors.Count);
+        }
+
+        [Test]
+        public void EnrollShouldThrowExceptionWhenWarriorNameIsRepeated()
+        {
+            _arena.Enroll(new Warrior("Pesho", 10, 30));
+
+            Assert.Throws<InvalidOperationException>(() => _arena.Enroll(new Warrior("Pesho", 10, 30)), "Warrior is already enrolled for the fights!");
+            Assert.AreEqual(1, _arena.Count);
+            Assert.AreEqual(1, _arena.Warriors.Count);
+        }
+        #endregion
+
+        #region Figth Tests
+        [TestCase("Pesho", 20, 60, "Gosho", 20, 60)]
+        [TestCase("Gosho", 15, 90, "Tosho", 20, 80)]
+        [TestCase("Tosho", 20, 150, "Losho", 10, 70)]
+        public void FightShouldWorkWithValidparameters(string attackerName, int attackerDamage, int attackerHp, string defenderName, int defenderDamage, int defenderHp)
+        {
+            Warrior attacker = new Warrior(attackerName, attackerDamage, attackerHp);
+            Warrior defender = new Warrior(defenderName, defenderDamage, defenderHp);
+
+            _arena.Enroll(attacker);
+            _arena.Enroll(defender);
+
+            _arena.Fight(attackerName, defenderName);
+
+            Assert.AreEqual(defenderHp - attackerDamage, defender.HP);
+        }
+
+        [TestCase("Pesho", 20, 60, "Gosho", 20, 60)]
+        [TestCase("Gosho", 15, 90, "Tosho", 20, 80)]
+        [TestCase("Tosho", 20, 150, "Losho", 10, 70)]
+        public void FightShouldThrowExceptionWhenWarriorNameIsMissing(string attackerName, int attackerDamage, int attackerHp, string defenderName, int defenderDamage, int defenderHp)
+        {
+            Warrior attacker = new Warrior(attackerName, attackerDamage, attackerHp);
+            Warrior defender = new Warrior(defenderName, defenderDamage, defenderHp);
+
+            _arena.Enroll(attacker);
+            _arena.Enroll(defender);
+
+            string missingWarrior = "Ivancho";
+
+            Assert.Throws<InvalidOperationException>(() => _arena.Fight(attackerName, missingWarrior));
+        }
+        #endregion
     }
 }
--- OOP/Unit Testing/04. Fighting Arena/FightingArena.Tests/WarriorTests.cs
+++ OOP/Unit Testing/04. Fighting Arena/FightingArena.Tests/WarriorTests.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
 
 namespace FightingArena.Tests
 {
@@ -11,6 +9,77 @@ namespace FightingArena.Tests
         //You should test all the constructors
         //You should test all properties(getters and setters)
         //You should test all the methods and validations inside the class}
+        [TestCase("Mage", 35, 100)]
+        [TestCase("Tank", 55, 250)]
+        [TestCase("Rogue", 75, 80)]
+        public void ConstructorShouldWorkWithValidParameters(string name, int damage, int hp)
+        {
+            Warrior warrior = new Warrior(name, damage, hp);
 
+            Assert.AreEqual(name, warrior.Name);
+            Assert.AreEqual(damage, warrior.Damage);
+            Assert.AreEqual(hp, warrior.HP);
+        }
+
+        #region Setter Tests
+        [TestCase(null, 35, 100)]
+        [TestCase("", 55, 250)]
+        [TestCase(" ", 75, 80)]
+        public void ConstructorShouldThrowExceptionWithInvalidName(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, damage, hp), "Name should not be empty or whitespace!");
+        }
+
+        [TestCase("Mage", 0, 100)]
+        [TestCase("Tank", -1, 250)]
+        [TestCase("Rogue", -1421, 80)]
+        public void ConstructorShouldThrowExceptionWithInvalidDamage(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, damage, hp), "Damage value should be positive!");
+        }
+
+        [TestCase("Mage", 35, -1)]
+        [TestCase("Tank", 55, -9)]
+        [TestCase("Rogue", 75, -1421)]
+        public void ConstructorShouldThrowExceptionWithInvalidHp(string name, int damage, int hp)
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior(name, damage, hp), "HP should not be negative!");
+        }
+        #endregion
+
+        #region Attack Tests
+        [TestCase(29)]
+        [TestCase(15)]
+        [TestCase(1)]
+        public void AttackShouldThrowExceptionWhenThisHpIsBelowThreshold(int hp)
+        {
+            Warrior thisWarrior = new Warrior("Pesho", 25, hp);
+            Warrior otherWarrior = new Warrior("Gosho", 25, 100);
+
+            Assert.Throws<InvalidOperationException>(() => thisWarrior.Attack(otherWarrior), "Your HP is too low in order to attack other warriors!");
+        }
+
+        [TestCase(29)]
+        [TestCase(15)]
+        [TestCase(1)]
+        public void AttackShouldThrowExceptionWhenOtherHpIsBelowThreshold(int hp)
+        {
+            Warrior thisWarrior = new Warrior("Pesho", 25, 100);
+            Warrior otherWarrior = new Warrior("Gosho", 25, hp);
+
+            Assert.Throws<InvalidOperationException>(() => thisWarrior.Attack(otherWarrior));
+        }
+
+        [TestCase(100)]
+        [TestCase(200)]
+        [TestCase(300)]
+        public void AttackShouldThrowExceptionWhenThisHpIsLessThanOtherDamage(int damage)
+        {
+            Warrior thisWarrior = new Warrior("Pesho", 25, damage - 10);
+            Warrior otherWarrior = new Warrior("Gosho", damage, 100);
+
+            Assert.Throws<InvalidOperationException>(() => thisWarrior.Attack(otherWarrior));
+        }
+        #endregion
     }
 }
