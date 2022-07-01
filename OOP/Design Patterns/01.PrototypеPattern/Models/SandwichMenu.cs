4b63def7d4a26800f19a360fcf4b1abf9c9d100e
Banovvv <ivan_banov@abv.bg>
2022-07-01 14:43:05 +0300

    More classes

---
+++ OOP/Design Patterns/01.PrototypеPattern/Models/SandwichMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace _01.PrototypеPattern.Models
+{
+    public partial class SandwichMenu
+    {
+        public SandwichMenu()
+        {
+            Sandwiches = new List<SandwichMenuItem>();
+        }
+
+        public List<SandwichMenuItem> Sandwiches { get; private set; }
+
+        public void Add(string name, SandwichPrototype sandwich)
+        {
+            Sandwiches.Add(new SandwichMenuItem(name, sandwich));
+        }
+    }
+}
---
+++ OOP/Design Patterns/01.PrototypеPattern/Models/SandwichMenuItem.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.PrototypеPattern.Models
+{
+    public partial class SandwichMenu
+    {
+        public class SandwichMenuItem
+        {
+            public SandwichMenuItem(string name, SandwichPrototype sandwich)
+            {
+                Name = name;
+                Sandwich = sandwich;
+            }
+
+            public string Name { get; private set; }
+            public SandwichPrototype Sandwich { get; private set; }
+
+        }
+    }
+}
