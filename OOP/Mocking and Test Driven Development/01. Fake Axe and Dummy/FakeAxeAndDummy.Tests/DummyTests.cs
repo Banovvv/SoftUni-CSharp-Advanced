ed149f3aa4092423e25a6e92d91886e29d40bd98
Banovvv <ivan_banov@abv.bg>
2022-07-10 21:22:57 +0300

    Dummy Tests

---
+++ OOP/Mocking and Test Driven Development/01. Fake Axe and Dummy/FakeAxeAndDummy.Tests/DummyTests.cs
@@ -0,0 +1,31 @@
+using FakeAxeAndDummy.Models;
+using NUnit.Framework;
+using System;
+
+namespace FakeAxeAndDummy.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(15)]
+        [TestCase(99)]
+        [TestCase(100)]
+        public void DummyInitiliazationShouldWorkWithValidParameters(int health)
+        {
+            Dummy dummy = new Dummy(health);
+            Assert.AreEqual(health, dummy.Health);
+        }
+
+        [TestCase(-1)]
+        [TestCase(-10)]
+        [TestCase(-15)]
+        [TestCase(990)]
+        [TestCase(101)]
+        public void DummyInitiliazationShouldNotWorkWithInvalidParameters(int health)
+        {
+            Assert.Throws<ArgumentException>(() => new Dummy(health));
+        }
+    }
+}
