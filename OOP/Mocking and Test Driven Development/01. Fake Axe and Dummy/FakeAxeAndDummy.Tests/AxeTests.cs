9807481b2a3bb2878bc2a1bafccd6fb005f32b41
Banovvv <ivan_banov@abv.bg>
2022-07-11 14:44:44 +0300

    Axe valid param tests

--- OOP/Mocking and Test Driven Development/01. Fake Axe and Dummy/FakeAxeAndDummy.Tests/AxeTests.cs
+++ OOP/Mocking and Test Driven Development/01. Fake Axe and Dummy/FakeAxeAndDummy.Tests/AxeTests.cs
@@ -8,9 +8,16 @@ namespace FakeAxeAndDummy.Tests
     [TestFixture]
     public class AxeTests
     {
+        [TestCase(1, 1)]
+        [TestCase(5, 10)]
+        [TestCase(25, 50)]
+        [TestCase(49, 99)]
+        [TestCase(50, 100)]
         public void AxeInitiliazationShouldWorkWithValidParameters(int damage, int durability)
         {
             IWeapon axe = new Axe(damage, durability);
+            Assert.AreEqual(damage, axe.Damage);
+            Assert.AreEqual(durability, axe.Durability);
         }
     }
 }
