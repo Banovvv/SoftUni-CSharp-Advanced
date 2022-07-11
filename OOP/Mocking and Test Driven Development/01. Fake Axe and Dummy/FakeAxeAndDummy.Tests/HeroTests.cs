9077f135df195b776b99cd9937970fc72fb0afa0
Banovvv <ivan_banov@abv.bg>
2022-07-11 15:02:45 +0300

    Hero tests for null IWeapon

---
+++ OOP/Mocking and Test Driven Development/01. Fake Axe and Dummy/FakeAxeAndDummy.Tests/HeroTests.cs
@@ -0,0 +1,17 @@
+﻿using FakeAxeAndDummy.Interfaces;
+using FakeAxeAndDummy.Models;
+using NUnit.Framework;
+using System;
+
+namespace FakeAxeAndDummy.Tests
+{
+    [TestFixture]
+    public class HeroTests
+    {
+        [Test]
+        public void HeroInitializationShouldNotWorkWithInvalidWeapon()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Hero(null, 100), "Weapon cannot be null!");
+        }
+    }
+}
