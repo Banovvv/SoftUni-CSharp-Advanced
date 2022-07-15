6a14f2651e7783286338e174b9e4c322399e6879
Banovvv <ivan_banov@abv.bg>
2022-07-15 14:43:21 +0300

    Pilot done

--- OOP/Exam problems/01.Formula1/Models/Pilot.cs
+++ OOP/Exam problems/01.Formula1/Models/Pilot.cs
@@ -1,10 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Formula1.Models.Contracts;
+using System;
 
 namespace Formula1.Models
 {
-    internal class Pilot
+    public class Pilot
     {
+        private string fullName;
+        private IFormulaOneCar car;
+
+        public Pilot(string fullName)
+        {
+            FullName = fullName;
+            CanRace = false;
+            NumberOfWins = 0;
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException($"Invalid pilot model: {value}.");
+                }
+
+                fullName = value;
+            }
+        }
+
+        public bool CanRace { get; private set; }
+
+        public IFormulaOneCar Car
+        {
+            get => car;
+            private set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException("Pilot car can not be null.");
+                }
+                car = value;
+            }
+        }
+
+        public int NumberOfWins { get; private set; }
+
+        public void AddCar(IFormulaOneCar car)
+        {
+            Car = car;
+            CanRace = true;
+        }
+
+        public void WinRace()
+        {
+            NumberOfWins++;
+        }
     }
 }
