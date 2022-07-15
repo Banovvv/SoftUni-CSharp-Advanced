270726377e81d13667d348af2466704c4a4e29b5
Banovvv <ivan_banov@abv.bg>
2022-07-15 14:53:27 +0300

    Race

--- OOP/Exam problems/01.Formula1/Models/Race.cs
+++ OOP/Exam problems/01.Formula1/Models/Race.cs
@@ -1,10 +1,69 @@
-﻿using System;
+﻿using Formula1.Models.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Formula1.Models
 {
-    internal class Race
+    public class Race
     {
+        private string raceName;
+        private int numberOfLaps;
+
+        public Race(string raceName, int numberOfLaps)
+        {
+            RaceName = raceName;
+            NumberOfLaps = numberOfLaps;
+            TookPlace = false;
+        }
+
+        public string RaceName
+        {
+            get => raceName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException($"Invalid race name: {value}.");
+                }
+
+                raceName = value;
+            }
+        }
+
+        public int NumberOfLaps
+        {
+            get => numberOfLaps;
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException($"Invalid lap numbers: {value}.");
+                }
+
+                numberOfLaps = value;
+            }
+        }
+
+        public bool TookPlace { get; private set; }
+
+        public ICollection<IPilot> Pilots { get; private set; }
+
+        public void AddPilot(IPilot pilot)
+        {
+            Pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The {RaceName} race has:");
+            sb.AppendLine($"Participants: {Pilots.Count}");
+            sb.AppendLine($"Number of laps: {NumberOfLaps}");
+            sb.AppendLine($"Took place: {(TookPlace ? "Yes" : "No")}");
+
+            return sb.ToString().Trim();
+        }
     }
 }
