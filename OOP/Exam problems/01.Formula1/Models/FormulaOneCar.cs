d9dfbbd55954195fcaf979dbccce556414b9bfd4
Banovvv <ivan_banov@abv.bg>
2022-07-15 14:32:51 +0300

    Cars done

---
+++ OOP/Exam problems/01.Formula1/Models/FormulaOneCar.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Formula1.Models
+{
+    public abstract class FormulaOneCar
+    {
+        private string model;
+        private int horsepower;
+        private double engineDisplacement;
+
+        public FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            Model = model;
+            Horsepower = horsepower;
+            EngineDisplacement = engineDisplacement;
+        }
+
+        public string Model
+        {
+            get => model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException($"Invalid car model: {value}.");
+                }
+
+                model = value;
+            }
+        }
+
+        public int Horsepower
+        {
+            get => horsepower;
+            private set
+            {
+                if (value < 900 || value > 1050)
+                {
+                    throw new ArgumentException($"Invalid car horsepower: {value}.");
+                }
+
+                horsepower = value;
+            }
+        }
+
+        public double EngineDisplacement
+        {
+            get => engineDisplacement;
+            private set
+            {
+                if (value < 1.6 || value > 2.0)
+                {
+                    throw new ArgumentException($"Invalid car engine displacement: {value}.");
+                }
+
+                engineDisplacement = value;
+            }
+        }
+
+        public double RaceScoreCalculator(int laps)
+        {
+            return engineDisplacement / horsepower * laps;
+        }
+    }
+}
