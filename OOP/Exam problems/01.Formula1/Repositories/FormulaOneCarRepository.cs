8d4060a15f9b09e188e82363c43e2c177aaffd66
Banovvv <ivan_banov@abv.bg>
2022-07-15 15:41:13 +0300

    FormulaOneCarRepository

--- OOP/Exam problems/01.Formula1/Repositories/FormulaOneCarRepository.cs
+++ OOP/Exam problems/01.Formula1/Repositories/FormulaOneCarRepository.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Formula1.Models.Contracts;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Formula1.Repositories
 {
-    internal class FormulaOneCarRepository
+    public class FormulaOneCarRepository
     {
+        private readonly ICollection<IFormulaOneCar> models;
+
+        public ICollection<IFormulaOneCar> Models => models;
+
+        public void Add(IFormulaOneCar car)
+        {
+            models.Add(car);
+        }
+
+        public bool Remove(IFormulaOneCar car)
+        {
+            return models.Remove(car);
+        }
+
+        public IFormulaOneCar FindByName(string model)
+        {
+            return models.Where(x => x.Model == model).FirstOrDefault();
+        }
     }
 }
