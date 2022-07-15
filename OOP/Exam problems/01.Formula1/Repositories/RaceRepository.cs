fa7ea7a8b5f6524a7f920a8fbb4c13accdde28eb
Banovvv <ivan_banov@abv.bg>
2022-07-15 15:46:10 +0300

    RaceRepository

--- OOP/Exam problems/01.Formula1/Repositories/RaceRepository.cs
+++ OOP/Exam problems/01.Formula1/Repositories/RaceRepository.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Formula1.Models.Contracts;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Formula1.Repositories
 {
-    internal class RaceRepository
+    public class RaceRepository
     {
+        private readonly ICollection<IRace> models;
+
+        public ICollection<IRace> Models => models;
+
+        public void Add(IRace race)
+        {
+            models.Add(race);
+        }
+
+        public bool Remove(IRace race)
+        {
+            return models.Remove(race);
+        }
+
+        public IRace FindByName(string raceName)
+        {
+            return models.Where(x => x.RaceName == raceName).FirstOrDefault();
+        }
     }
 }
