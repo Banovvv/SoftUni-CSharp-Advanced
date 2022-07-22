d1d199d7ba7863b0883f3d3fb9471662f40263e7
Banovvv <ivan_banov@abv.bg>
2022-07-22 15:51:31 +0300

    Racers

---
+++ OOP/Exam problems/02.CarRacing/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,14 @@
+﻿using CarRacing.Models.Cars.Contracts;
+
+namespace CarRacing.Models.Racers
+{
+    public class ProfessionalRacer : Racer
+    {
+        private static readonly string racingBehavior = "strict";
+        private static readonly int drivingExperience = 30;
+
+        public ProfessionalRacer(string username, ICar car) : base(username, racingBehavior, drivingExperience, car)
+        {
+        }
+    }
+}
---
+++ OOP/Exam problems/02.CarRacing/Models/Racers/StreetRacer.cs
@@ -0,0 +1,14 @@
+﻿using CarRacing.Models.Cars.Contracts;
+
+namespace CarRacing.Models.Racers
+{
+    public class StreetRacer : Racer
+    {
+        private static readonly string racingBehavior = "aggressive";
+        private static readonly int drivingExperience = 10;
+
+        public StreetRacer(string username, ICar car) : base(username, racingBehavior, drivingExperience, car)
+        {
+        }
+    }
+}
