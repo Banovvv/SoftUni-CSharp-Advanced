3361a42ea94947dda5d765508392ba0a69aaa35a
Banovvv <ivan_banov@abv.bg>
2022-07-22 14:42:50 +0300

    Basic setup

---
+++ OOP/Exam problems/02.CarRacing/Program.cs
@@ -0,0 +1,13 @@
+﻿using CarRacing.Core;
+using CarRacing.Core.Contracts;
+namespace CarRacing
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
