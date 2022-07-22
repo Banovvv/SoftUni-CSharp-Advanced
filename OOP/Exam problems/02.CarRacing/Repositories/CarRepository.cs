75a1c652b79edcf0fa65a9099f2404020bc6a1ab
Banovvv <ivan_banov@abv.bg>
2022-07-22 16:44:39 +0300

    CarRepository.cs

---
+++ OOP/Exam problems/02.CarRacing/Repositories/CarRepository.cs
@@ -0,0 +1,36 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using CarRacing.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarRacing.Repositories
+{
+    public class CarRepository : IRepository<ICar>
+    {
+        private List<ICar> models = new List<ICar>();
+
+        public CarRepository()
+        {
+        }
+
+        public IReadOnlyCollection<ICar> Models => models.AsReadOnly();
+
+        public void Add(ICar model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentException("Cannot add null in Car Repository");
+            }
+
+            models.Add(model);
+        }
+
+        public ICar FindBy(string property) => models.Where(x => x.VIN == property).FirstOrDefault();
+
+        public bool Remove(ICar model)
+        {
+            return models.Remove(model);
+        }
+    }
+}
