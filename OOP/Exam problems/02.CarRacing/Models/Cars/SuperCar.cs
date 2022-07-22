5d70a3b6b405d8ce1b0f2f3040fc0261111a61a8
Banovvv <ivan_banov@abv.bg>
2022-07-22 15:13:36 +0300

    Car types

---
+++ OOP/Exam problems/02.CarRacing/Models/Cars/SuperCar.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRacing.Models.Cars
+{
+    public class SuperCar : Car
+    {
+        private static readonly double fuelAvailable = 80;
+        private static readonly double fuelConsumptionPerRace = 10;
+
+        public SuperCar(string make, string model, string vin, int horsePower) : base(make, model, vin, horsePower, fuelAvailable, fuelConsumptionPerRace)
+        {
+        }
+    }
+}
---
+++ OOP/Exam problems/02.CarRacing/Models/Cars/TunedCar.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRacing.Models.Cars
+{
+    public class TunedCar : Car
+    {
+        private static readonly double fuelAvailable = 65;
+        private static readonly double fuelConsumptionPerRace = 7.5;
+
+        public TunedCar(string make, string model, string vin, int horsePower) : base(make, model, vin, horsePower, fuelAvailable, fuelConsumptionPerRace)
+        {
+        }
+    }
+}
