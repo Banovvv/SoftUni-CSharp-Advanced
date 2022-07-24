cce2225fb6d39952257d29408d712fa3feae705c
Banovvv <ivan_banov@abv.bg>
2022-07-24 08:47:07 +0300

    GiftBox.cs

---
+++ OOP/Design Patterns/02.Composite/Models/GiftBox.cs
@@ -0,0 +1,16 @@
+﻿namespace _02.Composite.Models
+{
+    public abstract class GiftBox
+    {
+        protected string name;
+        protected int price;
+
+        public GiftBox(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public abstract int CalculateTotalPrice();
+    }
+}
