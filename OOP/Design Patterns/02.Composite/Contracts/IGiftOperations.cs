a864e3361c3547f7f91094ab21d1ddb37ffb0371
Banovvv <ivan_banov@abv.bg>
2022-07-24 08:49:13 +0300

    IGiftOperations

---
+++ OOP/Design Patterns/02.Composite/Contracts/IGiftOperations.cs
@@ -0,0 +1,10 @@
+﻿using _02.Composite.Models;
+
+namespace _02.Composite.Contracts
+{
+    public interface IGiftOperations
+    {
+        void Add(GiftBox gift);
+        void Remoce(GiftBox gift);
+    }
+}
