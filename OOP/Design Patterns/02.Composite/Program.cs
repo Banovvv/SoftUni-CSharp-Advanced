37c8c4729f2f6aca274940adc2b560f9f8945cec
Banovvv <ivan_banov@abv.bg>
2022-07-25 08:20:18 +0300

    Implementation

--- OOP/Design Patterns/02.Composite/Program.cs
+++ OOP/Design Patterns/02.Composite/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _02.Composite.Models;
+using System;
 
 namespace _02.Composite
 {
@@ -6,7 +7,21 @@ namespace _02.Composite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var phone = new Gift("Phone", 256);
+            phone.CalculateTotalPrice();
+            Console.WriteLine();
+
+            var giftBox = new CompositeGift("Gift box", 0);
+            var truckToy = new Gift("Truck toy", 289);
+            var soldierToy = new Gift("Soldier toy", 587);
+            giftBox.Add(truckToy);
+            giftBox.Add(soldierToy);
+            var childBox = new CompositeGift("Child box", 0);
+            var carToy = new Gift("Car toy", 200);
+            childBox.Add(carToy);
+            giftBox.Add(childBox);
+
+            Console.WriteLine($"Total price of this gift box is: {giftBox.CalculateTotalPrice()}");
         }
     }
 }
